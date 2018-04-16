cfa6711543eb4c463898bb2dcd582cc1856d809b
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-04-16 19:11:26 +0100

    can add info into all text boxes"

--- selenium_class_walkthrough/selenium_class.rb
+++ selenium_class_walkthrough/selenium_class.rb
@@ -9,5 +9,33 @@ class SeleniumQaToolsForm
         @chrome_driver = Selenium::WebDriver.for :chrome
     end 
 
+    def visit_practice_form
+        @chrome_driver.get(PRACTICE_FORM_URL)
+    end
+
+    def input_firstname_field(text)
+        @chrome_driver.find_element(:name, FIRST_NAME_FIELD_NAME).send_keys(text)
+        sleep 5
+    end
+
+    def input_lastname_field(text)
+        @chrome_driver.find_element(:name, LAST_NAME_FIELD_NAME).send_keys(text)
+        sleep 5
+    end
+    def input_date_field(text)
+        @chrome_driver.find_element(:id, "datepicker").send_keys(text)
+        sleep 5
+    end
+
+    def get_first_name_text
+        puts @chrome_driver.find_element(:name, FIRST_NAME_FIELD_NAME)['value']
+    end
+
 end 
 
+form = SeleniumQaToolsForm.new
+form.visit_practice_form
+# form.input_firstname_field('jeff')
+# form.input_lastname_field('thomas')
+form.input_date_field('yyyyyy')
+form.get_first_name_text
